2f4483dbf3509875386676b01ad13adf371f9380
fonsalir03 <weht03anon@gmail.com>
2025-01-20 17:57:07 -0600

    clean up code

--- src/home.js
+++ src/home.js
@@ -1,28 +1,18 @@
 import bakeryImagePath from "./assets/bakery.png"
 
 const generateHomeContent = (function(){
-    console.log(content)
-//create header elem
-//add relavent text to header
-const homeHeader = document.createElement("header");
-homeHeader.textContent = "dynamic header";
+    const homeHeader = document.createElement("header");
+    homeHeader.textContent = "dynamic header";
 
-//create p elem
-const homeText = document.createElement("p");
-homeText.textContent = "dynamic text";
-//add relavent text to p
+    const homeText = document.createElement("p");
+    homeText.textContent = "dynamic text";
 
-//import img
-const bakeryImage = document.createElement("img");
-bakeryImage.src = bakeryImagePath;
+    const bakeryImage = document.createElement("img");
+    bakeryImage.src = bakeryImagePath;
 
-
-//append header to div#content
-content.appendChild(homeHeader);
-//append img to div#content
-content.appendChild(bakeryImage)
-//append p to div#content
-content.appendChild(homeText);
+    content.appendChild(homeHeader);
+    content.appendChild(bakeryImage)
+    content.appendChild(homeText);
 });
 
 export {generateHomeContent};
