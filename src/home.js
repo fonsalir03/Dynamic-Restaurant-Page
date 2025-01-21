440db7abeb9efc6996ab8d151e9ec8678915e599
fonsalir03 <weht03anon@gmail.com>
2025-01-20 18:16:18 -0600

    remove content div from index and create one in home module

--- src/home.js
+++ src/home.js
@@ -1,18 +1,32 @@
 import bakeryImagePath from "./assets/bakery.png"
 
 const generateHomeContent = (function(){
+    const content = document.createElement("div");
+    content.style.display = "flex";
+    content.style.flexDirection = "column";
+    content.style.alignItems = "center"
+
+    content.style.display = "flex";
+    content.style.flexDirection = "column";
+    content.style.alignItems = "center"
+
     const homeHeader = document.createElement("header");
     homeHeader.textContent = "dynamic header";
 
     const homeText = document.createElement("p");
     homeText.textContent = "dynamic text";
+    homeText.style.textAlign = "center";
+    homeText.style.fontSize = "large";
 
     const bakeryImage = document.createElement("img");
     bakeryImage.src = bakeryImagePath;
+    bakeryImage.style.width = "10%"
 
     content.appendChild(homeHeader);
     content.appendChild(bakeryImage)
     content.appendChild(homeText);
+
+    document.body.appendChild(content)
 });
 
 export {generateHomeContent};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./styles.css";
 import {generateHomeContent} from "./home.js"
 
-let content = document.querySelector("#content");
 generateHomeContent()
