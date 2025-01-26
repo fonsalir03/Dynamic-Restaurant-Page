f3bc244fc9f313fe7e6bcddbd45b53af2e37232b
fonsalir03 <weht03anon@gmail.com>
2025-01-25 18:37:04 -0600

    add generateAboutContent module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./styles.css";
-import {generateHomeContent} from "./home.js"
+import { generateHomeContent } from "./home.js";
 import { generateMenuContent } from "./menu.js";
+import { generateAboutContent } from "./about.js";
