817a6d2ea498e972f9d7d769c28dcf93fd998362
fonsalir03 <weht03anon@gmail.com>
2025-01-26 00:56:24 -0600

    add tab switching logic

--- src/index.js
+++ src/index.js
@@ -2,3 +2,31 @@ import "./styles.css";
 import { generateHomeContent } from "./home.js";
 import { generateMenuContent } from "./menu.js";
 import { generateAboutContent } from "./about.js";
+
+
+//add event listener for each header nav button
+//  the event listeners will delete the parent container and add the content
+
+//home button
+const homeButton = document.querySelector("#homeButton");
+//menu button
+const menuButton = document.querySelector("#menuButton");
+//about button
+const aboutButton = document.querySelector("#aboutButton");
+
+homeButton.addEventListener("click", function(){
+    let content = document.querySelector("#content");
+    content.remove();
+    generateHomeContent();
+})
+
+menuButton.addEventListener("click", function(){
+    let content = document.querySelector("#content");
+    content.remove();
+    generateMenuContent();
+})
+aboutButton.addEventListener("click", function(){
+    let content = document.querySelector("#content");
+    content.remove();
+    generateAboutContent();
+})
