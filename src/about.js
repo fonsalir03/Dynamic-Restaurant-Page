c2fc8c6e62a4a77da7d410d48c67d1eb835ae6d9
fonsalir03 <weht03anon@gmail.com>
2025-01-25 18:36:09 -0600

    add function to genereate about page content

---
+++ src/about.js
@@ -0,0 +1,6 @@
+const generateAboutContent = (function(){
+    //be sure to add styles after ALL alements are created and children of the appropriate parents
+
+    //create div#content container
+
+});
